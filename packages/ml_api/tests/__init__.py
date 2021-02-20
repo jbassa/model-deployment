9f0351bcbd52908abb8a162fc6eaff5af0cd3876
Joel <joel_bassa@yahoo.fr>
2021-02-20 14:31:56 -0500

    init file

--- packages/ml_api/tests/__init__.py
+++
@@ -1,5 +0,0 @@
-from api.config import PACKAGE_ROOT
-from regression_model.config import config
-
-with open(PACKAGE_ROOT / 'VERSION') as version_file:
-    __version__ = version_file.read().strip()
